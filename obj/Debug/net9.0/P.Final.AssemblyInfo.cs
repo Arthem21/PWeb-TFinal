9c9920c7a8fb001169a8519bf1c793f42e1cbafb
astralsandwich6324 <santillangarciaj659@gmail.com>
2024-12-08 16:54:21 -0400

    ListadoMultas Fixed

--- obj/Debug/net9.0/P.Final.AssemblyInfo.cs
+++ obj/Debug/net9.0/P.Final.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P.Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb732b982335a2cdc63a7a48b622500496e79785")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a20c30ac9816f0ad113c1ab363da3cd3b1782840")]
 [assembly: System.Reflection.AssemblyProductAttribute("P.Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P.Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
