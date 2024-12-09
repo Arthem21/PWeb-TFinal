aa505f8e5cb4ecbfbaf0dc43dc0ddfed2b0cff98
astralsandwich6324 <santillangarciaj659@gmail.com>
2024-12-09 10:26:06 -0400

    Modulo Oficina central

--- obj/Debug/net9.0/P.Final.AssemblyInfo.cs
+++ obj/Debug/net9.0/P.Final.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P.Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c9920c7a8fb001169a8519bf1c793f42e1cbafb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ed9a0250be714f9d06b3a72ab34adfec0f87766")]
 [assembly: System.Reflection.AssemblyProductAttribute("P.Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P.Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
